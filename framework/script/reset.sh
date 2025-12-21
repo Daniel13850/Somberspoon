520c367a7fb7a23ac2a9eee106c8cd9c86a1b1f0
Daniel13850 <notzoldjulian2003@gmail.com>
2025-12-21 11:03:58 +0100

    fix

--- framework/script/reset.sh
+++ framework/script/reset.sh
@@ -46,4 +46,4 @@ sed -i "0,/^[[:space:]]*clone-remote-port:/{s/^\([[:space:]]*\)clone-remote-port
 sed -i "0,/^[[:space:]]*port:/{s/^\([[:space:]]*\)port:.*/\1port: ${bedrock_port_if_no_clone}/}" ~/server/plugins/Geyser-Spigot/config.yml
 sed -i "0,/^[[:space:]]*enabled:/{s/^\([[:space:]]*\)enabled:.*/\1enabled: ${discord_log_enabled}/}" ~/server/plugins/DiscordLog/config.yml
 sed -i "0,/^[[:space:]]*webhook:/{s|^\([[:space:]]*\)webhook:.*|\1webhook: ${discord_log_webhook}|}" ~/server/plugins/DiscordLog/config.yml
-echo $essentials_motd > ~/server/plugins/Essentials/motd.txt
+cp ~/motd.txt > ~/server/plugins/Essentials/motd.txt
